9a39abbd520b0848d4db4b1cfc6d23571d73e013
Sajag Pradhanang <spd.sajag@gmail.com>
2021-07-25 18:40:56 +0545

    select date added

--- CompileUI.ps1
+++ CompileUI.ps1
@@ -9,7 +9,7 @@ foreach ($file in $fileList){
     if ($extn -eq ".ui"){
         $currentFile = $file | % {[System.IO.Path]::GetFileNameWithoutExtension($_)}
         $rawFile = $currentFile+".ui"
-        #$finalFile = "ui_"+$currentFile+".py"
+        #$finalFile = "ui"+$currentFile+".py"
         $TempFile = $currentFile+".py"
         pyside2-uic $rawFile > $TempFile
         $FinalFile = "ui_" + $TempFile
@@ -18,13 +18,14 @@ foreach ($file in $fileList){
         #$TempFile | % {[System.Io.File]::ReadAllText($TempFile)} | Out-File -FilePath $FinalFile -Encoding utf8
         Remove-Item $TempFile
     }
-    
+
     if ($extn -eq ".qrc"){
         $currentFile = $file | % {[System.IO.Path]::GetFileNameWithoutExtension($_)}
         $rawFile = $currentFile+".qrc"
-        $TempFile = $currentFile+".py"
-        pyside2-rcc $rawFile -o $TempFile
-        $FinalFile = "ui_" + $TempFile
+        $TempFile1 = $currentFile
+        pyside2-rcc $rawFile -o $TempFile1
+        TFile = $TempFile1 | % {[System.IO.Path]::GetFileNameWithoutExtension($_)} + "_rc" 
+        $FinalFile = $TFile + ".py"
         Get-Content -Encoding Unicode $TempFile | Out-File -Encoding utf8 $FinalFile
         #Get-Content -Encoding Unicode $TempFile | Out-File -Encoding utf8NoBOM $FinalFile
         #$TempFile | % {[System.Io.File]::ReadAllText($TempFile)} | Out-File -FilePath $FinalFile -Encoding utf8
