f2bf2ba7f6c511aa4dccc849f9a58274f44b82a3
dikshya28 <68811571+dikshya28@users.noreply.github.com>
2021-08-18 11:43:40 +0545

    Update statistics(OLD).py

--- statistics(OLD).py
+++ statistics(OLD).py
@@ -1,3 +1,4 @@
+
 import sys
 from matplotlib.backends.backend_qt5 import FigureCanvasQT
 import numpy as np
@@ -103,8 +104,8 @@ class Canvas_N_Cases_mnth(FigureCanvasQTAgg):
 
         plt.plot(Data_s, newCases, 'o',label = 'NewCases')
         plt.plot(Data_s, newCases, '-')
-        plt.plot(extention_s, y_pred, '-',label = 'Predictions')
-        plt.plot(extention_s, y_pred)
+        plt.plot(Data_s, y_pred, '-',label = 'Predictions')
+
 
         plt.title('Covid Cases latest Month')
         plt.xlabel('Date')
@@ -133,13 +134,12 @@ class Canvas_N_Cases_week(FigureCanvasQTAgg):
         data = pd.read_csv('Resources/CovidDataNepal.csv',usecols= ['date','new_cases'])
         #converting Date to proper datatype (DateTime)
         data['date'] = pd.to_datetime(data['date']) 
-        
 
         #Getting Last week
         lastWeek = datetime.now()+relativedelta(weeks=-1)
 
         #Structuring start and end Date
-        start_date = datetime.strftime(lastMnth,"%Y-%m-%d")
+        start_date = datetime.strftime(lastWeek,"%Y-%m-%d")
         end_date = datetime.strftime(datetime.now(),"%Y-%m-%d")
 
         #Filtering data using mask
@@ -151,8 +151,8 @@ class Canvas_N_Cases_week(FigureCanvasQTAgg):
         
         #making an array of dates
         LatestWeekData['date'] = pd.to_numeric(pd.to_datetime(LatestWeekData['date']) )
-        list_Date = LatestWeekData['date'].tolist()
-        Data = np.array(list_Date).reshape((-1,1))
+        list_Data = LatestWeekData['date'].tolist()
+        Data = np.array(list_Data).reshape((-1,1))
 
         #Making a second array of dates
         LatestWeekData_s['date'] = pd.to_datetime(LatestWeekData_s['date'])
@@ -160,41 +160,24 @@ class Canvas_N_Cases_week(FigureCanvasQTAgg):
         Data_s = np.array(list_Data_s)
 
         #making a list of y cases
-        list_newCases = LatestMonthData['new_cases'].tolist()
+        list_newCases = LatestWeekData['new_cases'].tolist()
         newCases = np.array(list_newCases)
 
-        #fitting a regression model
+        TempCount = 0
+        for i in Data_s:
+            i = datetime.strftime(i,"%m-%d")
+            Data_s[TempCount] = i
+            TempCount += 1
+
+        #fitting a regression Model
         model = LinearRegression().fit(Data, newCases)
 
-        #getting latest date from data
-        latest = np.amax(Data_s, axis = 0)
-
-        #extending date upto
-        extended = latest + relativedelta(weeks=+1)
-        count = latest
-        list_extension = []
-
-        #making extra x axis data points
-        while count <= extended:
-                list_extension.append(count)
-                count = count + relativedelta(days=+1)
-
-        tem = pd.to_numeric(pd.to_datetime(list_extension))
-        extension = np.array(tem).reshape((-1,1))
-        extension_s = np.array(list_extension)
-
         #making a prediction
-        y_pred = model.predict(extension)
-
-        
-        temp_r = datetime.strftime(datetime.now(),"%Y-%m-%d")
-        temp = datetime.strptime(temp_r,"%Y-%m-%d")
-
+        y_pred = model.predict(Data)
+    
         plt.plot(Data_s, newCases, 'o',label = 'NewCases')
         plt.plot(Data_s, newCases, '-')
-        plt.plot(extention_s, y_pred, '-',label = 'Predictions')
-        plt.plot(extention_s, y_pred)
-
+        plt.plot(Data_s, y_pred, '-',label = 'Predictions')
 
         plt.title('Covid Cases latest Week')
         plt.xlabel('Date')
@@ -223,10 +206,10 @@ class Canvas_N_Cases_overall(FigureCanvasQTAgg):
         data = pd.read_csv('Resources/CovidDataNepal.csv',usecols= ['date','total_cases'])
         #converting Date to proper datatype (DateTime)
         data['date'] = pd.to_datetime(data['date']) 
-        
 
         #Because the data is from 25 Jan 2020 AD
-        Begining = datetime.date(2020,1,25)
+        #Begining = datetime.date(2020,1,25)
+        Begining = datetime.strptime("2020-01-25","%Y-%m-%d")        
 
         #Structuring start and end Date
         start_date = datetime.strftime(Begining,"%Y-%m-%d")
@@ -236,68 +219,29 @@ class Canvas_N_Cases_overall(FigureCanvasQTAgg):
         mask = (data['date'] > start_date) & (data['date'] <= end_date) 
 
         #Applying mask
-        LatestWeekData = data.loc[mask]
-        LatestWeekData_s = data.loc[mask]
+        LatestMonthData = data.loc[mask]
         
         #making an array of dates
-        LatestWeekData['date'] = pd.to_numeric(pd.to_datetime(LatestWeekData['date']) )
-        list_Date = LatestWeekData['date'].tolist()
-        Data = np.array(list_Date).reshape((-1,1))
+        Date = LatestMonthData['date'].tolist()
 
-        #Making a second array of dates
-        LatestWeekData_s['date'] = pd.to_datetime(LatestWeekData_s['date'])
-        list_Data_s = LatestWeekData_s['date'].tolist()
-        Data_s = np.array(list_Data_s)
+        TotalCases = LatestMonthData['total_cases'].tolist()
+        plt.plot(Date, TotalCases, 'o',label = 'TotalCases')
+        plt.plot(Date, TotalCases, '-')
 
-        #making a list of y cases
-        list_newCases = LatestMonthData['new_cases'].tolist()
-        newCases = np.array(list_newCases)
-
-        #fitting a regression model
-        model = LinearRegression().fit(Data, newCases)
-
-        #getting latest date from data
-        latest = np.amax(Data_s, axis = 0)
-
-        #extending date upto
-        extended = latest + relativedelta(months=+1)
-        count = latest
-        list_extension = []
-
-        #making extra x axis data points
-        while count <= extended:
-                list_extension.append(count)
-                count = count + relativedelta(days=+1)
-
-        tem = pd.to_numeric(pd.to_datetime(list_extension))
-        extension = np.array(tem).reshape((-1,1))
-        extension_s = np.array(list_extension)
-
-        #making a prediction
-        y_pred = model.predict(extension)
+        plt.title('Total Covid Cases Overall')
+        plt.xlabel('Date')
+        plt.ylabel('Total Cases')
 
         
-        temp_r = datetime.strftime(datetime.now(),"%Y-%m-%d")
-        temp = datetime.strptime(temp_r,"%Y-%m-%d")
-
-        plt.plot(Data_s, newCases, 'o',label = 'NewCases')
-        plt.plot(Data_s, newCases, '-')
-        plt.plot(extention_s, y_pred, '-',label = 'Predictions')
-        plt.plot(extention_s, y_pred)
-
-        plt.title('Covid Cases Overall')
-        plt.xlabel('Date')
-        plt.ylabel('Active Cases')
 
         plt.legend()
         plt.grid(True)
 
         plt.tight_layout() # works for small screen; solves padding issues
 
-
 ##########################################################################################################
         # Nepal Deaths Months
-##########################################################################################################
+##########################################################################################################        
 
 class Canvas_N_Deaths_mnth(FigureCanvasQTAgg):
     def __init__(self, parent):
@@ -313,7 +257,6 @@ class Canvas_N_Deaths_mnth(FigureCanvasQTAgg):
         data = pd.read_csv('Resources/CovidDataNepal.csv',usecols= ['date','new_deaths'])
         #converting Date to proper datatype (DateTime)
         data['date'] = pd.to_datetime(data['date']) 
-        
 
         #Getting Last month
         lastMnth = datetime.now()+relativedelta(months=-1)
@@ -327,53 +270,19 @@ class Canvas_N_Deaths_mnth(FigureCanvasQTAgg):
 
         #Applying mask
         LatestMonthData = data.loc[mask]
-        LatestMonthData_s = data.loc[mask]
         
         #making an array of dates
-        LatestMonthData['date'] = pd.to_numeric(pd.to_datetime(LatestMonthData['date']) )
-        list_Date = LatestMonthData['date'].tolist()
-        Data = np.array(list_Date).reshape((-1,1))
+        Date = LatestMonthData['date'].tolist()
 
-        #Making a second array of dates
-        LatestMonthData_s['date'] = pd.to_datetime(LatestMonthData_s['date'])
-        list_Data_s = LatestMonthData_s['date'].tolist()
-        Data_s = np.array(list_Data_s)
-
-        #making a list of y cases
-        list_newdeaths = LatestMonthData['new_deaths'].tolist()
-        newCases = np.array(list_newdeaths)
-
-        #fitting a regression model
-        model = LinearRegression().fit(Data, newDeaths)
-
-        #getting latest date from data
-        latest = np.amax(Data_s, axis = 0)
-
-        #extending date upto
-        extended = latest + relativedelta(months=+1)
-        count = latest
-        list_extension = []
-
-        #making extra x axis data points
-        while count <= extended:
-                list_extension.append(count)
-                count = count + relativedelta(days=+1)
-
-        tem = pd.to_numeric(pd.to_datetime(list_extension))
-        extension = np.array(tem).reshape((-1,1))
-        extension_s = np.array(list_extension)
-
-        #making a prediction
-        y_pred = model.predict(extension)
-
+        TempCount = 0
+        for i in Date:
+            i = datetime.strftime(i,"%m-%d")
+            Date[TempCount] = i
+            TempCount += 1
         
-        temp_r = datetime.strftime(datetime.now(),"%Y-%m-%d")
-        temp = datetime.strptime(temp_r,"%Y-%m-%d")
-
-        plt.plot(Data_s, newDeaths, 'o',label = 'NewDeaths')
-        plt.plot(Data_s, newDeaths, '-')
-        plt.plot(extention_s, y_pred, '-',label = 'Predictions')
-        plt.plot(extention_s, y_pred)
+        newDeaths = LatestMonthData['new_deaths'].tolist()
+        plt.plot(Date, newDeaths, 'o',label = 'NewDeaths')
+        plt.plot(Date, newDeaths, '-')
 
         plt.title('Covid Deaths latest Month')
         plt.xlabel('Date')
@@ -402,10 +311,9 @@ class Canvas_N_Deaths_week(FigureCanvasQTAgg):
         data = pd.read_csv('Resources/CovidDataNepal.csv',usecols= ['date','new_deaths'])
         #converting Date to proper datatype (DateTime)
         data['date'] = pd.to_datetime(data['date']) 
-        
 
-        #Getting Last week
-        lastWeek = datetime.now()+relativedelta(weeks=-1)
+        #Getting Last month
+        lastMnth = datetime.now()+relativedelta(weeks=-1)
 
         #Structuring start and end Date
         start_date = datetime.strftime(lastMnth,"%Y-%m-%d")
@@ -415,55 +323,20 @@ class Canvas_N_Deaths_week(FigureCanvasQTAgg):
         mask = (data['date'] > start_date) & (data['date'] <= end_date) 
 
         #Applying mask
-        LatestWeekData = data.loc[mask]
-        LatestWeekData_s = data.loc[mask]
+        LatestMonthData = data.loc[mask]
         
         #making an array of dates
-        LatestWeekData['date'] = pd.to_numeric(pd.to_datetime(LatestWeekData['date']) )
-        list_Date = LatestWeekData['date'].tolist()
-        Data = np.array(list_Date).reshape((-1,1))
+        Date = LatestMonthData['date'].tolist()
 
-        #Making a second array of dates
-        LatestWeekData_s['date'] = pd.to_datetime(LatestWeekData_s['date'])
-        list_Data_s = LatestWeekData_s['date'].tolist()
-        Data_s = np.array(list_Data_s)
-
-        #making a list of y cases
-        list_newDeaths = LatestMonthData['new_deaths'].tolist()
-        newDeaths = np.array(list_newDeaths)
-
-        #fitting a regression model
-        model = LinearRegression().fit(Data, newDeaths)
-
-        #getting latest date from data
-        latest = np.amax(Data_s, axis = 0)
-
-        #extending date upto
-        extended = latest + relativedelta(weeks=+1)
-        count = latest
-        list_extension = []
-
-        #making extra x axis data points
-        while count <= extended:
-                list_extension.append(count)
-                count = count + relativedelta(days=+1)
-
-        tem = pd.to_numeric(pd.to_datetime(list_extension))
-        extension = np.array(tem).reshape((-1,1))
-        extension_s = np.array(list_extension)
-
-        #making a prediction
-        y_pred = model.predict(extension)
-
+        TempCount = 0
+        for i in Date:
+            i = datetime.strftime(i,"%m-%d")
+            Date[TempCount] = i
+            TempCount += 1
         
-        temp_r = datetime.strftime(datetime.now(),"%Y-%m-%d")
-        temp = datetime.strptime(temp_r,"%Y-%m-%d")
-
-        plt.plot(Data_s, newDeaths, 'o',label = 'NewDeaths')
-        plt.plot(Data_s, newDeaths, '-')
-        plt.plot(extention_s, y_pred, '-',label = 'Predictions')
-        plt.plot(extention_s, y_pred)
-
+        newDeaths = LatestMonthData['new_deaths'].tolist()
+        plt.plot(Date, newDeaths, 'o',label = 'NewDeaths')
+        plt.plot(Date, newDeaths, '-')
 
         plt.title('Covid Deaths latest Week')
         plt.xlabel('Date')
@@ -492,10 +365,11 @@ class Canvas_N_Deaths_overall(FigureCanvasQTAgg):
         data = pd.read_csv('Resources/CovidDataNepal.csv',usecols= ['date','total_deaths'])
         #converting Date to proper datatype (DateTime)
         data['date'] = pd.to_datetime(data['date']) 
-        
 
         #Because the data is from 25 Jan 2020 AD
-        Begining = datetime.date(2020,1,25)
+        #Begining = datetime.date(2020,1,25)
+        Begining = datetime.strptime("2020-01-25","%Y-%m-%d")
+        
 
         #Structuring start and end Date
         start_date = datetime.strftime(Begining,"%Y-%m-%d")
@@ -505,64 +379,27 @@ class Canvas_N_Deaths_overall(FigureCanvasQTAgg):
         mask = (data['date'] > start_date) & (data['date'] <= end_date) 
 
         #Applying mask
-        LatestWeekData = data.loc[mask]
-        LatestWeekData_s = data.loc[mask]
+        LatestMonthData = data.loc[mask]
         
         #making an array of dates
-        LatestWeekData['date'] = pd.to_numeric(pd.to_datetime(LatestWeekData['date']) )
-        list_Date = LatestWeekData['date'].tolist()
-        Data = np.array(list_Date).reshape((-1,1))
+        Date = LatestMonthData['date'].tolist()
+        
+        totalDeaths = LatestMonthData['total_deaths'].tolist()
+        plt.plot(Date, totalDeaths, 'o',label = 'TotalDeaths')
+        plt.plot(Date, totalDeaths, '-')
 
-        #Making a second array of dates
-        LatestWeekData_s['date'] = pd.to_datetime(LatestWeekData_s['date'])
-        list_Data_s = LatestWeekData_s['date'].tolist()
-        Data_s = np.array(list_Data_s)
+        plt.title('Total Covid Deaths Overall')
+        plt.xlabel('Date')
+        plt.ylabel('Total Deaths')
 
-        #making a list of y deaths
-        list_newCases = LatestMonthData['new_deaths'].tolist()
-        newCases = np.array(list_newDeaths)
-
-        #fitting a regression model
-        model = LinearRegression().fit(Data, newDeaths)
-
-        #getting latest date from data
-        latest = np.amax(Data_s, axis = 0)
-
-        #extending date upto
-        extended = latest + relativedelta(months=+1)
-        count = latest
-        list_extension = []
-
-        #making extra x axis data points
-        while count <= extended:
-                list_extension.append(count)
-                count = count + relativedelta(days=+1)
-
-        tem = pd.to_numeric(pd.to_datetime(list_extension))
-        extension = np.array(tem).reshape((-1,1))
-        extension_s = np.array(list_extension)
-
-        #making a prediction
-        y_pred = model.predict(extension)
+        #plt.xticks(ticker.AutoLocator())       #This didn't work due to empty values
+        
 
         
-        temp_r = datetime.strftime(datetime.now(),"%Y-%m-%d")
-        temp = datetime.strptime(temp_r,"%Y-%m-%d")
-
-        plt.plot(Data_s, newDeaths, 'o',label = 'NewDeaths')
-        plt.plot(Data_s, newDeaths, '-')
-        plt.plot(extention_s, y_pred, '-',label = 'Predictions')
-        plt.plot(extention_s, y_pred)
-
-        plt.title('Covid Deaths Overall')
-        plt.xlabel('Date')
-        plt.ylabel('Deaths')
-
         plt.legend()
         plt.grid(True)
 
         plt.tight_layout() # works for small screen; solves padding issues
-
 
 ##########################################################################################################
         # Nepal Total Vaccination
